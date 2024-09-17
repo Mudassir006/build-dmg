634e79e4b85bc53c5f38c5844e31600e5d4011f7
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2024-09-17 21:50:23 +0500

    Update TTrumps.py

--- TTrumps.py
+++ TTrumps.py
@@ -14,33 +14,33 @@ jobs:
 
     steps:
     - name: Checkout code
-      uses: actions/checkout@v3  # Check out the code from the repository
+      uses: actions/checkout@v4 
 
     - name: Set up Python
-      uses: actions/setup-python@v4  # Set up Python
+      uses: actions/setup-python@v4  
       with:
-        python-version: '3.12'  # Replace with the Python version you need
+        python-version: '3.12'  
 
     - name: Install dependencies
       run: |
         python -m pip install --upgrade pip
-        pip install -r requirements.txt  # Install dependencies from requirements.txt
+        pip install -r requirements.txt 
 
     - name: Install py2app
       run: |
-        python -m pip install py2app  # Install py2app
+        python -m pip install py2app 
 
     - name: Build macOS app using py2app
       run: |
-        python setup.py py2app  # Build the app using py2app
+        python setup.py py2app  
 
     - name: Adjust permissions
       run: |
-        chmod -R 755 dist/TTrumps.app  # Ensure correct permissions
+        chmod -R 755 dist/TTrumps.app  
 
     - name: Create .dmg file
       run: |
-        hdiutil create -volname MyApp -srcfolder dist/ -ov -format UDZO MyApp.dmg  # Package the app into a .dmg
+        hdiutil create -volname MyApp -srcfolder dist/ -ov -format UDZO MyApp.dmg 
 
     - name: Upload Artifact
       uses: actions/upload-artifact@v3
